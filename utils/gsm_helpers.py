7ba60bac98fa7d6093ce9b8b0dd9482cc1b5193e
Olga Mirete <olgamirete@gmail.com>
2022-01-01 17:57:07 +0100

    testing

--- utils/gsm_helpers.py
+++ utils/gsm_helpers.py
@@ -7,14 +7,14 @@ SERIAL_PORT = "/dev/ttyAMA0"
 GPIO.setmode(GPIO.BOARD)
 
 def _write_to_serial(ser: serial.Serial, command: str):
-    ser.write(command.encode())
+    ser.write(command.encode('utf-8'))
 
 def get_unread_sms():
     #Open port with baud rate
     with serial.Serial(SERIAL_PORT, baudrate=9600, timeout=5) as piSerial:
         sleep(1)
         print("Will write to serial.")
-        _write_to_serial(piSerial, 'AT+CMGL="REC UNREAD"')
+        _write_to_serial(piSerial, 'AT+CMGL="REC UNREAD"\r\n')
         print("Written to serial.")
         # The response should be something like this:
 
@@ -22,8 +22,10 @@ def get_unread_sms():
         # Reading text messages is easy.
         # +CMGL: 2,"REC UNREAD","+85291234567",,"07/02/18,00:07:22+32"
                 
-        response_lines = piSerial.readlines()
+        # while True:
+            # print(piSerial.readline())
         
+        response_lines = piSerial.readlines()
         print(f"received {len(response_lines)} responses. Detail:")
 
         for x in response_lines:
