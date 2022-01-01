a3b345809a04d554bd10d51743bda4c4ff713798
Olga Mirete <olgamirete@gmail.com>
2022-01-02 00:41:42 +0100

    general improvements

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 import utils.telegram_helpers as telegram_helpers
 import utils.gsm_helpers as gsm_helpers
 
-# while True:
-#     try:
+def handle_read_sms():
+    sms_type = ''
+    while sms_type.lower() not in ['all', 'read', 'unread']:
         
-#     except:
-#         pass
+        sms_type = sms_type.lower()
+        sms_type = input('Read All / Read / Unread? ')
 
-gsm_helpers.open_serial_terminal()
+        if sms_type == 'all':
+            sms_type = gsm_helpers.SMS_STATUS.ALL
+        elif sms_type == 'read':
+            sms_type = gsm_helpers.SMS_STATUS.READ
+        elif sms_type == 'unread':
+            sms_type = gsm_helpers.SMS_STATUS.UNREAD
+        
+        gsm_helpers.read_sms(sms_type)
+        
+def handle_send_command():
+    cmd = input('Enter command to send: ')
+    output = gsm_helpers.send_command(cmd)
+    print(output.text())
+
+options = [
+    ["Open serial terminal", gsm_helpers.open_serial_terminal],
+    ["Read sms", handle_read_sms],
+    ["Send command", handle_send_command]
+]
+
+formatted_options = [f"{str(i+1)}. {options[i][0]}" for i in range(len(options))]
+formatted_options = '\n'.join(formatted_options)
+
+while True:
+
+    x = input('Choose an option:\n' + formatted_options)
+
+    options[int(x)]()
 
 # telegram_helpers.send_message("test, holis3")
 
--- utils/gsm_helpers.py
+++ utils/gsm_helpers.py
@@ -1,22 +1,41 @@
 import serial
 import RPi.GPIO as GPIO
 from time import sleep
+from dotenv import load_dotenv
+from os import getenv
 
-SERIAL_PORT = "/dev/ttyAMA0"
+load_dotenv()
+
+SERIAL_PORT = getenv("SERIAL_PORT")
+
+class SMS_STATUS:
+    ALL = "ALL"
+    READ = "REC READ"
+    UNREAD = "REC UNREAD"
+
+class AT_COMMAND_OUTPUT:
+    def __init__(self) -> None:
+        self.status = None
+        self._lines = []
+    
+    def setStatus(self, newStatus: str):
+        self.status = newStatus
+    
+    def addLine(self, newLine: str):
+        self._lines.append(newLine)
+
+    def text(self):
+        return '\n'.join(self._lines)
 
 GPIO.setmode(GPIO.BOARD)
 
 class GSMInitializationError(Exception):
     pass
 
-# class GSMError(Exception):
-#     pass
-
 def _write_to_serial(ser: serial.Serial, command: str):
     ser.write(command.encode('utf-8'))
 
 def open_serial_terminal():
-    #Open port with baud rate
     with serial.Serial(SERIAL_PORT, baudrate=9600, timeout=5) as piSerial:
         sleep(3)
         _write_to_serial(piSerial, 'AT\r\n')
@@ -59,3 +78,50 @@ def reset_module():
     confirm = input('Do you really want to reset the GSM Module? (y/n): ')
     if confirm.lower() == 'y':
         raise NotImplemented
+
+def send_command(cmd: str, encoding_for_decoding: str = 'utf-8'):
+
+    if not cmd.endswith('\r\n'):
+        cmd += '\r\n'
+    cmd = cmd.encode('utf-8')
+
+    with serial.Serial(SERIAL_PORT, baudrate=9600, timeout=5) as piSerial:
+        sleep(3)
+        _write_to_serial(piSerial, 'AT\r\n')
+        sleep(1)
+        while True:
+            line = piSerial.readline().decode('utf-8')
+            if line == 'OK\r\n':
+                # Correctly initialized communication with GSM module!
+                break
+            if line == 'ERROR\r\n':
+                raise GSMInitializationError
+            sleep(1)
+        
+        _write_to_serial(piSerial, cmd)
+        sleep(1)
+
+        output = AT_COMMAND_OUTPUT()
+        line_constructor = ''
+        while True:
+            serial_str = piSerial.readline().decode(encoding_for_decoding)
+            if serial_str.endswith('\r\n'):
+                line_constructor += serial_str[:-2]
+                line = line_constructor
+                line_constructor = ''
+                output.addLine(line)
+                if serial_str == 'OK\r\n':
+                    output.setStatus('OK')
+                    break
+                if serial_str == 'ERROR\r\n':
+                    output.setStatus('ERROR')
+                    break
+            else:
+                line_constructor += serial_str
+                # Still constructing output, waiting for \\r\\n chars...
+            sleep(1)
+        return output
+
+def read_sms(filter_by_status: SMS_STATUS = SMS_STATUS.UNREAD):
+    output = send_command(f'AT+CMGL="{filter_by_status}"')
+    print(output.text())
