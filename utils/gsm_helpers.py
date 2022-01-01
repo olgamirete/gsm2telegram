47a85a75523161e2132bc86559d3d82501398f70
Olga Mirete <olgamirete@gmail.com>
2022-01-01 18:51:53 +0100

    improved serial response reading

--- utils/gsm_helpers.py
+++ utils/gsm_helpers.py
@@ -31,15 +31,21 @@ def get_unread_sms():
             # 'AT+CMGL="REC UNREAD"\r\n'
             
             _write_to_serial(piSerial, f'{cmd}\r\n')
-            print("Written to serial!")
+            # print("Written to serial!")
 
+            line_constructor = ''
             while True:
-                line = piSerial.readline().decode('utf-8')
-                print(line)
-                if line == 'OK\r\n':
-                    break
-                if line == 'ERROR\r\n':
-                    break
+                line_constructor += piSerial.readline().decode('utf-8')
+                if line_constructor.endswith('\n\r'):
+                    line = line_constructor[:-2]
+                    line_constructor = ''
+                    print(line)
+                    if line == 'OK':
+                        break
+                    if line == 'ERROR':
+                        break
+                else:
+                    print('Still constructing output, waiting for \\n\\r chars...')
                 sleep(1)
 
             cmd = input('Insert command (or press enter to quit): ')
