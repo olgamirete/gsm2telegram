49cc60bc5e3e0ce60d27b8a4ad52a12bd6f166b2
Olga Mirete <olgamirete@gmail.com>
2022-01-03 19:13:21 +0100

    testing

--- recordings/upload_recording.py
+++
@@ -1,3 +0,0 @@
-import utils.telegram_helpers as telegram_helpers
-
-telegram_helpers.send_document('test_call_1.wav')
--- upload_recording.py
+++ upload_recording.py
@@ -1,3 +1,3 @@
 import utils.telegram_helpers as telegram_helpers
 
-telegram_helpers.send_document('./recordings/test_call_1.wav')
+telegram_helpers.send_document('./recordings/test_call_2.wav')
