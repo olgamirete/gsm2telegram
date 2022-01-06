583a83bb19948670f5fc206a42a833163df0b266
Olga Mirete <olgamirete@gmail.com>
2022-01-06 21:15:34 +0100

    deleted tests file

--- tests.py
+++
@@ -1,5 +0,0 @@
-import utils.telegram_helpers as telegram_helpers
-
-# print(telegram_helpers.find_user_id_by_msg('test1235'))
-# telegram_helpers.send_message('test')
-telegram_helpers.send_document('~/gsm2telegram/recordings/test_call_1.wav')
