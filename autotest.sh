2818b0e458be396e37b65ec5fd8d10658fc17af1
Federico Giancarelli <olgamirete@gmail.com>
2022-01-06 22:14:41 +0100

    modified recording step

--- autotest.sh
+++ autotest.sh
@@ -1,4 +1,4 @@
 cd recordings
-sudo arecord sample.wav -c1 --vumeter=mono -d 5 --device=hw:1,0 --format S16_LE --rate 44100
+sudo arecord sample.wav --format S16_LE --rate 44100 -d 5 --vumeter=stereo
 cd ..
 python upload_recording.py
