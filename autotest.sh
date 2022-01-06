e27f75fa8bcdd90da2f9f85c5e961466c54068a6
Olga Mirete <olgamirete@gmail.com>
2022-01-06 22:42:52 +0100

    bugfix

--- autotest.sh
+++ autotest.sh
@@ -1,4 +1,4 @@
-python -c'from utils.sim800L_helpers import send_command;send_command("ATA")'
+python -c'from utils.sim800l_helpers import send_command;send_command("ATA")'
 cd recordings
 sudo arecord sample.wav --format S16_LE --rate 44100 -d 5 --vumeter=stereo
 cd ..
