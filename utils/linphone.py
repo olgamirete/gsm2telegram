d9b5285540fda2112438944d6802f6ce97f6b199
Olga Mirete <olgamirete@gmail.com>
2022-01-07 19:41:48 +0100

    added a wait statement after shell initialization

--- utils/linphone.py
+++ utils/linphone.py
@@ -6,6 +6,7 @@ class Linphone():
     def __init__(self, default_call_address: str = None) -> None:
         self.default_call_address = default_call_address
         self.p = Popen('linphonec')
+        self.p.wait()
     
     def answer_call(self):
         self.p.communicate('answer')
