90f926254c3bc9a6013a31bd27d4de6453e93c51
Olga Mirete <olgamirete@gmail.com>
2022-01-07 20:21:12 +0100

    bugfix

--- utils/linphone.py
+++ utils/linphone.py
@@ -6,25 +6,25 @@ from time import sleep
 class Linphone():
     def __init__(self, default_call_address: str = None) -> None:
         self.default_call_address = default_call_address
-        self.p = Popen('linphonec', shell=True)
+        self.p = Popen('linphonecsh init')
         self.p.wait()
     
     def answer_call(self):
-        self.p.communicate('answer')
+        self.p.communicate('linphonecsh generic "answer"')
     
     def call(self, sip_address: str = None):
         call_address = self.default_call_address | sip_address
-        self.p.communicate(f'call {call_address}')
+        self.p.communicate(f'linphonecsh generic "call {call_address}"')
 
 
 load_dotenv()
 MY_SIP_ADDRESS = getenv('MY_SIP_ADDRESS')
 print(f'sip address: {MY_SIP_ADDRESS}')
 
-# voip = Linphone(MY_SIP_ADDRESS)
+voip = Linphone(MY_SIP_ADDRESS)
 
 # sleep(5)
 
-# voip.call()
+voip.call()
 
-system(f'linphonec;call {MY_SIP_ADDRESS}')
+# system(f'linphonec;call {MY_SIP_ADDRESS}')
