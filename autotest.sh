11478879e5982c8d14bd08dd66b0a2fc9c91ed12
Olga Mirete <olgamirete@gmail.com>
2022-01-08 12:18:45 +0100

    testing

--- autotest.sh
+++ autotest.sh
@@ -1,9 +1,8 @@
 python -c'from utils.sim800l_helpers import send_command;send_command("ATA")'
+cd test_sounds
+aplay harp.wav
+cd ..
 cd recordings
 arecord sample.wav --format S16_LE --rate 44100 -d 5 --vumeter=stereo -D plughw:1,0
-cd ..
-python upload_recording.py
-cd recordings
-arecord sample.wav --format S16_LE --rate 44100 -d 5 --vumeter=stereo -D hw:1,0
 cd ..
 python upload_recording.py
