a898238139f89e95499153d00ce9f9f2bbe7fdbf
Olga Mirete <olgamirete@gmail.com>
2022-01-08 15:34:42 +0100

    changed python alias to "py"

--- autotest.sh
+++ autotest.sh
@@ -1,8 +1,8 @@
-python -c'from utils.sim800l_helpers import send_command;send_command("ATA")'
+py -c'from utils.sim800l_helpers import send_command;send_command("ATA")'
 cd test_sounds
 aplay harp.wav -D plughw:1,0
 cd ..
 cd recordings
 arecord sample.wav --format S16_LE --rate 44100 -d 5 --vumeter=stereo -D plughw:1,0
 cd ..
-python upload_recording.py
+py upload_recording.py
