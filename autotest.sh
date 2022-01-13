534541424154bdb152283f1ac65c87301ea2bc32
Olga Mirete <olgamirete@gmail.com>
2022-01-13 19:57:47 +0100

    updated autotest

--- autotest.sh
+++ autotest.sh
@@ -2,7 +2,7 @@ python3 -c'from utils.sim800l_helpers import send_command;send_command("ATA")'
 # aplay test_sounds/harp.wav -D plughw:1,0
 # arecord sample.wav --format S16_LE --rate 44100 -d 5 --vumeter=stereo -D plughw:1,0
 echo 'will begin recording...'
-arecord sample.wav -f cd -d 5 -D plughw:1,0
+arecord sample.wav -f cd -d 5 -D plughw:1,0  --vumeter=mono -c1
 echo 'will begin playing...'
 aplay sample.wav -D plughw:1,0
 # python3 upload_recording.py
